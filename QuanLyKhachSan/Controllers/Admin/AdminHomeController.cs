b3ffda259dfce4d3384c5c1eb3db266e6efd9311
Your Name <you@example.com>
2022-04-14 02:12:36 +0700

    first commit

---
+++ QuanLyKhachSan/Controllers/Admin/AdminHomeController.cs
@@ -0,0 +1,27 @@
+﻿using QuanLyKhachSan.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace QuanLyKhachSan.Controllers.Admin
+{
+    public class AdminHomeController : Controller
+    {
+        // GET: AdminHome
+        public ActionResult Index()
+        {
+            User user = (User)Session["ADMIN"];
+            if (user == null)
+            {
+                return RedirectToAction("Login", "AdminAuthentication");
+            }
+            else
+            {
+                return View();
+            }
+
+        }
+    }
+}
